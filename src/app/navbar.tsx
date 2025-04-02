a7907ead1ddf84b28e24d9dbaa00873d2d8ba6f1
Ahmed Haroon <ahmed.dys99@gmail.com>
2025-04-03 03:01:41 +0500

    first section

---
+++ src/app/navbar.tsx
@@ -0,0 +1,36 @@
+import { HambergerMenu } from "iconsax-react";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <div className="mt-8 lg:mt-10 bg-black/95 w-11/12 lg:w-2/3 mx-auto rounded-3xl z-50 relative">
+      <div className="flex flex-row justify-between items-center px-8 py-4">
+        <div className="text-white text-sm font-medium">Alter Labs</div>
+
+        <div className="hidden md:flex flex-row items-center justify-center gap-8">
+          <div className="text-white text-sm cursor-pointer hover:text-gray-300 transition-colors">Home</div>
+          <div className="text-white text-sm cursor-pointer hover:text-gray-300 transition-colors">About</div>
+          <div className="text-white text-sm cursor-pointer hover:text-gray-300 transition-colors">Services</div>
+          <div className="text-white text-sm cursor-pointer hover:text-gray-300 transition-colors">Contact</div>
+        </div>
+
+        <button className="md:hidden text-white" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <HambergerMenu className="w-6 h-6 stroke-white cursor-pointer" />
+        </button>
+      </div>
+
+      {isMenuOpen && (
+        <div className="mt-1 md:hidden bg-black/95 rounded-3xl py-4 px-8 absolute w-full">
+          <div className="flex flex-col space-y-4">
+            <div className="text-white text-sm py-2 cursor-pointer hover:text-gray-300 transition-colors">Home</div>
+            <div className="text-white text-sm py-2 cursor-pointer hover:text-gray-300 transition-colors">About</div>
+            <div className="text-white text-sm py-2 cursor-pointer hover:text-gray-300 transition-colors">Services</div>
+            <div className="text-white text-sm py-2 cursor-pointer hover:text-gray-300 transition-colors">Contact</div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,92 @@
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
+import { Activity, Bubble, Data, HambergerMenu, PathTool, PenTool2, People } from "iconsax-react";
+import { useState } from "react";
+import Navbar from "./navbar";
 
 export default function HomePage() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="h-screen bg-purple-50/30 w-full">
+        <div className="absolute top-1/3 left-1/4 w-80 h-80 bg-green-300 opacity-50 blur-[100px] rounded-full -z-10"></div>
+
+        <div className="absolute top-1/2 right-1/3 w-80 h-80 bg-purple-300 opacity-50 blur-[100px] rounded-full -z-10"></div>
+
+        <div className="absolute bottom-1/3 left-1/2 w-80 h-80 bg-orange-300 opacity-50 blur-[100px] rounded-full -z-10"></div>
+
+        <Navbar />
+
+        <div className="flex flex-col items-center justify-center gap-8 text-center mx-auto h-[calc(100vh-25%)]">
+          <div className="text-2xl lg:text-3xl font-bold w-full lg:w-1/3 leading-snug">
+            Deep Learning, Machine Learning, and Reinforcement Learning
+          </div>
+          <div className="text-sm lg:text-lg leading-loose w-full lg:w-1/2">
+            We work with 🏝️ independent creators, 🚀 tech startups and 💼 traditional businesses to create AI powered
+            solutions that are tailored to their needs.
+          </div>
+          <div className="pt-8">
+            <Link href="/contact" className="inline-block">
+              <button className="bg-black text-white px-8 py-3 text-sm rounded-full">Get in touch</button>
+            </Link>
+          </div>
         </div>
       </div>
+
+      {/* <div className="bg-white w-full h-screen py-16">
+        <div className="container mx-auto px-4">
+          <h2 className="text-2xl font-semibold text-center mb-16">Our Services</h2>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div className="bg-red-50 p-8 rounded-lg">
+              <div className="bg-white p-4 rounded-full w-16 h-16 flex items-center justify-center mb-4">
+                <People className="w-6 h-6 stroke-black" />
+              </div>
+              <h3 className="text-xl font-semibold mb-3">AI Consultancy</h3>
+              <p className="text-black text-sm leading-relaxed">
+                We provide expert consultancy on AI trends across industries and help identify the right AI technology
+                for your business.
+              </p>
+            </div>
+
+            <div className="bg-green-50 p-8 rounded-lg">
+              <div className="bg-white p-4 rounded-full w-16 h-16 flex items-center justify-center mb-4">
+                <Data className="w-6 h-6 stroke-black" />
+              </div>
+              <h3 className="text-xl font-semibold mb-3">Data Engineering</h3>
+              <p className="text-black text-sm leading-relaxed">
+                We specialize in designing data management solutions for large-scale datasets and developing insightful
+                analytics dashboards.
+              </p>
+            </div>
+
+            <div className="bg-yellow-50 p-8 rounded-lg">
+              <div className="bg-white p-4 rounded-full w-16 h-16 flex items-center justify-center mb-4">
+                <PathTool className="w-6 h-6 stroke-black" />
+              </div>
+              <h3 className="text-xl font-semibold mb-3">Custom Models</h3>
+              <p className="text-black text-sm leading-relaxed">
+                We empower businesses by creating custom machine learning models tailored to your specific needs and
+                trained on your data.
+              </p>
+            </div>
+
+            <div className="bg-blue-50 p-8 rounded-lg">
+              <div className="bg-white p-4 rounded-full w-16 h-16 flex items-center justify-center mb-4">
+                <Activity className="w-6 h-6 stroke-black" />
+              </div>
+              <h3 className="text-xl font-semibold mb-3">ML Operations</h3>
+              <p className="text-black text-sm leading-relaxed">
+                We design and maintain cost-effective, scalable production infrastructures for seamless ML model
+                deployment.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </main>
   );
 }
